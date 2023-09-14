6f30ab8f3ca058d0bfcb637f256cce44aaee13dd
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-09-15 00:43:10 +0200

    add sidebar, navbar, router...

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import "antd/dist/reset.css";
+import "antd/dist/reset.css"; //v4
 
 ReactDOM.render(
   <Router>
