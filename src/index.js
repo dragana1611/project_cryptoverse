62042679c36feb23d6ed380d20c7e6614e644cfe
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-09-16 22:16:00 +0200

    fixed api

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import "antd/dist/reset.css"; //v4
-// import store from "./app/store";
+import store from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    {/* <Provider> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
-
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,66 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { message } from "antd";
+
+// const cryptoHeaders = {
+//   "X-RapidAPI-Key": "a68eb5ace8msh0278d1c065a1300p1d8b16jsne97d8d61872c",
+//   "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+// };
+
+// const baseUrl = "https://api.coinranking.com/v2";
+// const createRequest = (url) => ({ url, headers: cryptoHeaders });
+
+//  export const cryptoApi = createApi({
+//   reducerPath: "cryptoApi",
+//   baseQuery: fetchBaseQuery({ baseUrl }),
+//   endpoints: (builder) => {
+//     getCryptos: builder.query({
+//       query: () => createRequest("/coin/"),
+//     });
+//   },
+// } || {});
+
+// export const { useGetCryptosQuery } = cryptoApi;
+
+//redux toolkit creates a hook that we can call instantly to get all the data for our query
+
+const cryptoApiHeaders = {
+  // 'X-RapidAPI-Host': RAPIDAPI_HOST,
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "b1d57f69b3mshadfcecfc9e489a6p14b18cjsn87ec366e56b2",
+};
+
+// const baseUrl = CRYPTO_API_URL;
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const creatRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "createApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => creatRequest(`/coins?limit=${count}`),
+    }),
+
+    getCryptoDetails: builder.query({
+      query: (coinId) => creatRequest(`/coin/${coinId}`),
+    }),
+
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) =>
+        creatRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+    }),
+    //
+    // Note: To access this endpoint you need premium plan
+    // getExchanges: builder.query({
+    //     query: () => createRequest('/exchanges'),
+    // }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  // useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
